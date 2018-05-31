70558fb6d061d73866ae711bd007e4ef42931e22
stormcloud266 <allprt89@gmail.com>
2018-05-31 08:19:00 -0600

    added start edit expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -43,6 +43,14 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates))
+    });
+  };
+};
+
 
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
